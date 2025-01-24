b41ce1103beb479d850c1a0dfe69bdcd9116e3e0
andreea-ionelaceban <andreea-ionela.ceban@student.howest.be>
2025-01-24 16:57:52 +0100

    added more css and gsap

--- src/main.js
+++ src/main.js
@@ -95,9 +95,83 @@ const backgroundColorChange = () => {
   })
 
 }
+
+const navigation = () => {
+  document.querySelector('.js').innerHTML = "Enabled";
+
+  const $nav = document.querySelector('.nav');
+  const $navButton = document.querySelector('.nav__button');
+  const $navList = document.querySelector('.nav__list');
+  const $iconLink = document.querySelector('#iconlink');
+  const listItems = $navList.querySelectorAll("li a");
+
+  $navButton.classList.remove('hidden');
+  $navList.classList.add("hidden");
+
+  const openNavigation = () => {
+    $navButton.setAttribute("aria-expanded", "true");
+    $iconLink.setAttribute("xlink:href", "#close");
+    $navList.classList.remove("hidden");
+    $nav.classList.add('nav--fixed');
+  }
+
+  const closeNavigation = () => {
+    $navButton.setAttribute("aria-expanded", "false");
+    $iconLink.setAttribute("xlink:href", "#navicon");
+    $navList.classList.add("hidden");
+    $nav.classList.remove('nav--fixed');
+  }
+
+  const toggleNavigation = () => {
+    const open = $navButton.getAttribute("aria-expanded");
+    open === "false" ? openNavigation() : closeNavigation();
+  }
+
+
+  const handleBlur = () => {
+    //if (!event.relatedTarget || !$navList.contains(event.relatedTarget)) {
+    closeNavigation();
+    //}
+  }
+
+  $navButton.addEventListener("click", toggleNavigation);
+
+  // add event to the last item in the nav list to trigger the disclosure to close if the user tabs out of the disclosure
+  listItems[listItems.length - 1].addEventListener("blur", handleBlur);
+
+  // Close the disclosure if a user presses the escape key
+  window.addEventListener("keyup", (e) => {
+    if (e.key === "Escape") {
+      $navButton.focus();
+      closeNavigation();
+    }
+  });
+}
+
+const wordsPopUp = () => {
+  // Wait until the DOM is loaded
+  document.addEventListener("DOMContentLoaded", () => {
+    // GSAP Animation
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: ".vision_section", // The section to trigger animation
+        start: "top center", // Animation starts when section top hits center of viewport
+      },
+    })
+      .to(".vision_one", { opacity: 1, y: 0, duration: 0.5 })
+      .to(".vision_two", { opacity: 1, y: 0, duration: 0.5 }, "-=0.3") // Overlap animations
+      .to(".vision_three", { opacity: 1, y: 0, duration: 0.5 }, "-=0.3")
+      .to(".vision_four", { opacity: 1, y: 0, duration: 0.5 }, "-=0.3");
+  });
+}
+
 const init = () => {
   travelScrollAnimation();
   backgroundColorChange();
+  navigation();
+  wordsPopUp();
 }
 init();
 
